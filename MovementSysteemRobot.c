3f3808484651373ac13c2f6dfea617e9736de453
Ayak26 <s1129531@windesheim.nl>
2019-03-14 13:01:57 +0100

    Update MovementSysteemRobot.c

--- MovementSysteemRobot.c
+++ MovementSysteemRobot.c
@@ -4,37 +4,42 @@ int E2 = 6;
 int M2 = 7;
 int snelheidM1 = 0; //set snelheid van motor 1, motor 1 is de linker rupsband
 int snelheidM2 = 0; // set snelheid van motor 2, motor 2 is de rechter rupsband
-
-void draaiL() {
-  int M1 = -250;
-  int M2 = 250;
+//laat robot om zijn as draaien
+void draaiL() { 
+  int M1 = -250; //Motor 1 draait achteruit
+  int M2 = 250;//Motor 2 draait vooruit
   links(M1);
   rechts(M2);
 }
+//draait alleen motor 1
 void draaiLinker(){
   int M1 = 250;
   int M2 = 0;
   links(M1);
   rechts(M2);
 }
+//Draait alleen motor 2
 void draaiRechter(){
   int M1 = 0;
   int M2 = 250;
   links(M1);
   rechts(M2);
 }
+//Laat robot om zijn as draaien
 void draaiR() {
   int M1 = 250;
   int M2 = -250;
   links(M1);
   rechts(M2);
 }
+//motor gaat rechtdoor
 void rechtdoor() {
   int M1 = 250;
   int M2 = 250;
   links(M1);
   rechts(M2);
 }
+//Zet een variabele voor de snelheid van de motoren
 int links(int i) {
   snelheidM1 = i;
 }
