7aa36bcf1a0918ad996bfc887a29ed9d88d2fe87
JtHove <s1094757@windesheim.nl>
2019-03-21 09:48:56 +0100

    verbetering

--- Lijnrobot_1.c
+++ Lijnrobot_1.c
@@ -2,7 +2,6 @@
 #define E1 5  //Motor rechts snelheid
 #define E2 6  //Motor links snelheid
 #define M2 7  //Motor links richting
-#define KNOP 2 //Aan/uit knop
 #define LS1 A0 //Ver links
 #define LS2 A1 //Midden links
 #define LS3 A2 //Midden rechts
@@ -14,7 +13,6 @@ void setup() {
   pinMode(M1, OUTPUT);
   pinMode(E2, OUTPUT);
   pinMode(M2, OUTPUT);
-  pinMode(KNOP, INPUT_PULLUP);
   pinMode(LS1, INPUT);
   pinMode(LS2, INPUT);
   pinMode(LS3, INPUT);
@@ -24,84 +22,35 @@ void setup() {
 }
 
 boolean on = false;
-boolean bruikbaar = true;
-int draai = 2000; //testen hoelang het duurt om 90 graden te draaien.
+int draai = 1000; //testen hoelang het duurt om 90 graden te draaien.
 int zwart = 800;
 int lospunt = 0;
 void loop() {
-  if (digitalRead(KNOP) == LOW && bruikbaar) {
-    on = !on;
-    bruikbaar = !bruikbaar;
-  }
   while (on) {
-    if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-      stopMotors();
-      Serial.write(1);
-    }
-    vooruit();
-    delay(2000);
-    if (analogRead(LS4) >= zwart) {
-      rechts();
-    }
-    vooruit();
-    if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-      stopMotors();
-      Serial.write(1);
-    }
-    vooruit();
-    if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-      links();
-    }
-    vooruit();
-    if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-      stopMotors();
-      Serial.write(1);
-    }
-    vooruit();
-    delay(2000):
-      if (analogRead(LS4) >= zwart && analogRead(LS5) >= zwart {
-    rechts();
-    }
-    vooruit();
-    if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-    stopMotors();
-      Serial.write(1);
-    }
-    vooruit();
-    if   (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart) {
-    links();
-    }
-    vooruit();
-    if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-    stopMotors();
-      Serial.write(1);
-    }
-
-
     if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart && analogRead(LS5) >= zwart) {
-    links();
+      links();
     } else if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-    stopMotors();
+      stopMotors();
       Serial.write(1);
     } else if (analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart && analogRead(LS5) >= zwart && lospunt % 2 != 0) {
-    rechts();
+      rechts();
     } else if (analogRead(LS2) >= zwart && analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-    rechts();
+      rechts();
     } else if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart && analogRead(LS3) >= zwart) {
-    links();
+      links();
     } else if (analogRead(LS2) >= zwart && analogRead(LS3) >= zwart) {
-    vooruit();
+      vooruit();
     } else if (analogRead(LS3) >= zwart && analogRead(LS4) >= zwart) {
-    bijstellenRechts();
+      bijstellenRechts();
     } else if (analogRead(LS1) >= zwart && analogRead(LS2) >= zwart) {
-    bijstellenLinks();
+      bijstellenLinks();
     }
   }
 }
 
 void motor1(int PWM, boolean vooruit) {
   analogWrite(E1, PWM);
-  if (vooruit) {
+  if (!vooruit) {
     digitalWrite(M1, HIGH);
   } else {
     digitalWrite(M1, LOW);
@@ -167,7 +116,9 @@ void serialEvent() {
     omdraaien();
   } else if (byteIn == 2) {
     achteruit();
-    delay(200); //testen
+    delay(1000); //testen
     omdraaien();
+  } else if (byteIn == 3) {
+    on = !on;
   }
 }
--- Lijnrobot_2.c
+++ Lijnrobot_2.c
@@ -1,34 +1,42 @@
-#define E3 4
-#define M3 5
+#define M3 4
+#define E3 5
 #define LDRpin A0
+#define KNOP 2 //Aan/uit knop
 
-int LDRValue = 0;     // result of reading the analog pin
+int LDRvalue = 0;     // result of reading the analog pin
 int lading = 0;
 boolean geladen = false;
 
 void setup() {
-  pinMode(2, OUTPUT);
+  pinMode(LDRpin, INPUT);
+  pinMode(KNOP, INPUT_PULLUP);
   pinMode(3, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(M3, OUTPUT);
   pinMode(E3, OUTPUT);
-  pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(KNOP), aanUit, LOW);
   Serial.begin(9600);
 }
 
+boolean on = false;
+boolean bruikbaar = true;
 void loop() {
-  LDRvalue = analogRead(A0);
-  if (geladen) {
-    l();
-    teller(lading);
-  }else {
-    e();
-    teller(lading);
+  while (on) {
+    LDRvalue = analogRead(A0);
+    if (geladen) {
+      l();
+      teller(lading);
+    } else {
+      e();
+      teller(lading);
+    }
   }
 }
 
@@ -43,7 +51,7 @@ void motor(int PWM, boolean omhoog) {
 
 void laad() {
   motor(200, true);
-  delay(200); //preciese delay testen
+  delay(2000); //preciese delay testen
   motor(0, false);
   lading++;
   geladen = !geladen;
@@ -52,48 +60,71 @@ void laad() {
 
 void los() {
   motor(200, false);
-  delay(200); //preciese delay testen
+  delay(2000); //preciese delay testen
   motor(0, true);
   geladen = !geladen;
   Serial.write(2);
 }
 
 void e() {
-  digitalWrite(2, LOW);
-  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
   digitalWrite(6, HIGH);
   digitalWrite(7, HIGH);
   digitalWrite(8, HIGH);
   digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(11, HIGH);
-  digitalWrite(12, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
 }
 
 void l() {
-  digitalWrite(2, LOW);
   digitalWrite(3, LOW);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
   digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(11, HIGH);
-  digitalWrite(12, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
 }
 
 void teller(int tel) {
-  if (tel==0) {
+  if (tel == 0) {
     nul();
-  } else if (tel==1) {
+  } else if (tel == 1) {
     een();
-  } else if (tel==2) {
+  } else if (tel == 2) {
     twee();
   }
 }
 
 void nul() {
-  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+
+void een() {
+  digitalWrite(3, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+}
+
+void twee() {
   digitalWrite(3, HIGH);
   digitalWrite(6, HIGH);
   digitalWrite(7, LOW);
@@ -101,31 +132,53 @@ void nul() {
   digitalWrite(9, HIGH);
   digitalWrite(10, HIGH);
   digitalWrite(11, LOW);
-  digitalWrite(12, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
 }
 
-void een() {
-  digitalWrite(2, HIGH);
+void acht() {
+  digitalWrite(3, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+}
+void ledUit() {
   digitalWrite(3, LOW);
   digitalWrite(6, LOW);
   digitalWrite(7, LOW);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
-  digitalWrite(11, LOW);
-  digitalWrite(12, HIGH);
-}
-
-void twee() {
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
-  digitalWrite(12, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+}
+void knipper() {
+  acht();
+  delay(300);
+  ledUit();
+  delay(300);
+}
+
+void aanUit() {
+  if (on) {
+    knipper();
+    knipper();
+    on = !on;
+    Serial.write(3);
+  } else {
+    knipper();
+    knipper();
+    knipper();
+    knipper();
+    on = !on;
+    Serial.write(3);
+  }
 }
 
 void serialEvent() {
